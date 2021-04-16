b27bce57ca3bbc552642ae88656ee14179944c01
Pete <peter.s.giles@gmail.com>
2021-04-16 12:25:54 +1000

    .

--- prepare.py
+++ prepare.py
@@ -1,22 +1,39 @@
 import glob
 import os
+import re
+from shutil import copyfile
+import os.path
 
 # detect the current working directory and print it
 path = os.getcwd()
 print("The current working directory is %s" % path)
 
-for file in glob.glob("to_zoia/*.bin"):
+for file in glob.glob("downloads/*.bin"):
 
     base = os.path.basename(file)
-    fileName = os.path.splitext(base)[0]   
+    fileName = os.path.splitext(base)[0].lower()
 
-    path = f'/patches/{fileName}'.lower()
+    fileName = re.sub(r'\s+', '_', fileName)
+    fileName = re.sub(r'^[0-9]+_zoia_', '', fileName)
+    fileName = fileName.strip()
 
-    print(path)
+    directory = f'./patches/{fileName}'
 
-    # try:
-    #     os.mkdir(path)
-    # except OSError:
-    #     print("Creation of the directory %s failed" % path)
-    # else:
-    #     print("Successfully created the directory %s " % path)
+    if(fileName):
+        try:
+            if(not os.path.exists(directory)):
+                os.mkdir(directory)
+
+            readme_path = f'{directory}/README.MD'
+            if(os.path.isfile(readme_path)):
+                readme = open(readme_path, "w")
+                pageTitle = fileName.replace("_", " ").capitalize()
+                readme.write(f'# {pageTitle}')
+
+            patch_path = f'{directory}/_zoia.bin'
+            if(not os.path.isfile(patch_path)):
+                copyfile(file, patch_path)
+
+        except OSError as error:
+            print(error)
+            print("Creation of the directory %s failed" % path)
